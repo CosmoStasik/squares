bba0eaf00ca247ba4b2b00e3d67c7ae7b74b5fc0
Stanislav Sobolevski <minuel.colt@gmail.com>
2022-08-13 12:38:32 +0300

    Initial Commit

---
+++ DZLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DZLayout
+//
+//  Created by Stanislav Sobolevsky on 13.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
